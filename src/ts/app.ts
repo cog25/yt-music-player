5bb74bc243e5159911fe0a6f9478b8c6e1456b0d
marshall-ku <marshall@kakao.com>
2020-11-03 13:42:29 +0900

    add background image

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -3,11 +3,21 @@ import { loadThumbnailById } from "./yt-functions";
 import { db, loadDB } from "./data";
 import { renderList, handleInput } from "./list";
 
+function backgroundImage() {
+    const imageLayer = document.createElement("div");
+
+    imageLayer.id = "bg";
+    imageLayer.style.backgroundImage = `url(${loadThumbnailById(db[0].id)})`;
+
+    document.body.append(imageLayer);
+}
+
 function init() {
     loadApi();
     loadDB();
     renderList();
     handleInput();
+    backgroundImage();
 }
 
 init();
