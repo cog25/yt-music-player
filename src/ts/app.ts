9c60b5e2d9e14d693698564b65a4e2ff7f3d47f5
marshall-ku <marshall@kakao.com>
2020-11-06 13:36:42 +0900

    remove useless imports

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,5 +1,5 @@
-import { player, loadApi, loadVideo, backgroundImage } from "./yt-player";
-import { db, loadDB } from "./data";
+import { loadApi } from "./yt-player";
+import { loadDB } from "./data";
 import { initializeList, handleInput } from "./list";
 
 function init() {
